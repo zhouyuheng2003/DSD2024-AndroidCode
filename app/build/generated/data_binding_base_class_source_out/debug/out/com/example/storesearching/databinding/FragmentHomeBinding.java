a32672d67a8d4214661dd2ff93c8e48f2abd2631
Henry <1287121642@qq.com>
2024-04-15 18:05:16 +0800

    4.15

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/storesearching/databinding/FragmentHomeBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/storesearching/databinding/FragmentHomeBinding.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -25,21 +27,26 @@ public final class FragmentHomeBinding implements ViewBinding {
   public final Button buttonSwitchSearchMode;
 
   @NonNull
-  public final SearchView mainSearchView;
+  public final LinearLayout linearlayoutSearchresult;
 
   @NonNull
-  public final TextView textHome;
+  public final ScrollView mainScrollView;
+
+  @NonNull
+  public final SearchView mainSearchView;
 
   @NonNull
   public final TextView textViewSearchMode;
 
   private FragmentHomeBinding(@NonNull ConstraintLayout rootView,
-      @NonNull Button buttonSwitchSearchMode, @NonNull SearchView mainSearchView,
-      @NonNull TextView textHome, @NonNull TextView textViewSearchMode) {
+      @NonNull Button buttonSwitchSearchMode, @NonNull LinearLayout linearlayoutSearchresult,
+      @NonNull ScrollView mainScrollView, @NonNull SearchView mainSearchView,
+      @NonNull TextView textViewSearchMode) {
     this.rootView = rootView;
     this.buttonSwitchSearchMode = buttonSwitchSearchMode;
+    this.linearlayoutSearchresult = linearlayoutSearchresult;
+    this.mainScrollView = mainScrollView;
     this.mainSearchView = mainSearchView;
-    this.textHome = textHome;
     this.textViewSearchMode = textViewSearchMode;
   }
 
@@ -76,15 +83,21 @@ public final class FragmentHomeBinding implements ViewBinding {
         break missingId;
       }
 
-      id = R.id.mainSearchView;
-      SearchView mainSearchView = ViewBindings.findChildViewById(rootView, id);
-      if (mainSearchView == null) {
+      id = R.id.linearlayout_searchresult;
+      LinearLayout linearlayoutSearchresult = ViewBindings.findChildViewById(rootView, id);
+      if (linearlayoutSearchresult == null) {
         break missingId;
       }
 
-      id = R.id.text_home;
-      TextView textHome = ViewBindings.findChildViewById(rootView, id);
-      if (textHome == null) {
+      id = R.id.mainScrollView;
+      ScrollView mainScrollView = ViewBindings.findChildViewById(rootView, id);
+      if (mainScrollView == null) {
+        break missingId;
+      }
+
+      id = R.id.mainSearchView;
+      SearchView mainSearchView = ViewBindings.findChildViewById(rootView, id);
+      if (mainSearchView == null) {
         break missingId;
       }
 
@@ -95,7 +108,7 @@ public final class FragmentHomeBinding implements ViewBinding {
       }
 
       return new FragmentHomeBinding((ConstraintLayout) rootView, buttonSwitchSearchMode,
-          mainSearchView, textHome, textViewSearchMode);
+          linearlayoutSearchresult, mainScrollView, mainSearchView, textViewSearchMode);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- app/src/main/java/com/example/storesearching/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/storesearching/ui/home/HomeFragment.java
@@ -12,11 +12,14 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.storesearching.databinding.FragmentHomeBinding;
 import android.widget.SearchView;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
 import android.widget.Button;
 import com.example.storesearching.R;
 import android.util.TypedValue;
 import android.util.Log;
 import android.graphics.Color;
+import androidx.navigation.Navigation;
 
 
 public class HomeFragment extends Fragment {
@@ -33,8 +36,8 @@ public class HomeFragment extends Fragment {
             SearchMode = 0;
             textView_SearchMode_content = new String[2];
             button_SwitchSearchMode_content = new String[2];
-            textView_SearchMode_content[0] = "stores";
-            textView_SearchMode_content[1] = "items";
+            textView_SearchMode_content[0] = "Stores";
+            textView_SearchMode_content[1] = "Items";
             button_SwitchSearchMode_content[0] = "store";
             button_SwitchSearchMode_content[1] = "item";
             update();
@@ -42,14 +45,16 @@ public class HomeFragment extends Fragment {
         private void update(){
             //搜索设置文本框
             TextView textView_SearchMode = root.findViewById(R.id.textView_SearchMode);
-            textView_SearchMode.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-            textView_SearchMode.setText("Search for " + textView_SearchMode_content[SearchMode]);
+            textView_SearchMode.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+            textView_SearchMode.setText("Search For " + textView_SearchMode_content[SearchMode]);
 
             //搜索模式切换按钮
             Button button_SwitchSearchMode = root.findViewById(R.id.button_SwitchSearchMode);
             button_SwitchSearchMode.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-            button_SwitchSearchMode.setText("Switch to " + button_SwitchSearchMode_content[SearchMode ^ 1]);
+//            button_SwitchSearchMode.setText("Switch to " + button_SwitchSearchMode_content[SearchMode ^ 1]);
+            button_SwitchSearchMode.setText("Switch");
             button_SwitchSearchMode.setBackgroundColor(Color.parseColor("#B9DADF"));
+
             button_SwitchSearchMode.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -73,8 +78,8 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         //初始化search mode
         HomeState HomeSearchMode = new HomeState();
@@ -87,8 +92,24 @@ public class HomeFragment extends Fragment {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                // 当用户提交查询文本时调用
-                // 在这里可以执行相应的搜索操作
+                LinearLayout linearlayout_searchresult = root.findViewById(R.id.linearlayout_searchresult);
+
+
+                View[] listItemViews = new View[20];
+                for (int i = 0; i < 20; i++) {
+                    View listItemView = inflater.inflate(R.layout.layout_listitem, null);
+                    Button button = listItemView.findViewById(R.id.button);
+                    button.setText("Button " + (i + 1));
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // 点击按钮后跳转到另一个 Fragment
+                            Navigation.findNavController(view).navigate(R.id.action_nav_home_to_storeFragment);
+                        }
+                    });
+                    listItemViews[i] = listItemView;
+                    linearlayout_searchresult.addView(listItemView);
+                }
                 return false;
             }
 
