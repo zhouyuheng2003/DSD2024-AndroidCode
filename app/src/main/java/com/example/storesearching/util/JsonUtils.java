912b11629e0f42fa42a8dbf1e9ee75c9f3e2d437
Henry <1287121642@qq.com>
2024-04-27 11:24:31 +0800

    Fix interface 7

--- app/src/main/java/com/example/storesearching/util/JsonUtils.java
+++ app/src/main/java/com/example/storesearching/util/JsonUtils.java
@@ -28,8 +28,8 @@ public class JsonUtils {
             jsonObject.put("state", myAddress.getAdminArea());
             jsonObject.put("city", myAddress.getLocality());
             jsonObject.put("street", myAddress.getThoroughfare());
-            jsonObject.put("number", null);
-            jsonObject.put("floor", null);
+            jsonObject.put("number", "");
+            jsonObject.put("floor", "");
             jsonObject.put("zipcode", myAddress.getPostalCode());
 
         } catch (JSONException e) {
